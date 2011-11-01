d5b81ef538cb1fea88d6ccae14180d60dbb4fef7
Miguel Guinada <mguinada@gmail.com>
2011-11-01 20:51:34 +0000

    Better scroll. Added js graph library.

--- view/js/web_socket_client.js
+++ view/js/web_socket_client.js
@@ -35,7 +35,18 @@ $(function() {
         tweets_view.append(tweet_view);
 
         tweet_view.slideDown(100);
-        $('html, body').animate({scrollTop: tweets_view.height()}, 800);
+        //$('html, body').animate({scrollTop: tweets_view.height()}, 800);
+        tweets_view.animate({scrollTop: $('div#tweet').length * 15}, 800);
+    }
+
+    function buildXAxis() {
+        var axis_values = [];
+
+        for(var i = 0; i < 60; i++) {
+            axis_values.push("" + pad(i, 1) + "m");
+        }
+
+        return axis_values;
     }
 });
 
@@ -43,3 +54,52 @@ function log(msg) {
     if (window.console) console.log(msg);
 }
 
+function pad(number, length) {
+    var str = '' + number;
+    while (str.length < length) {
+        str = '0' + str;
+    }
+    return str;
+}
+
+
+/*
+VER: http://www.highcharts.com/demo/
+var chart = new Highcharts.Chart({
+    chart: { renderTo: 'chart',
+             defaultSeriesType: 'line',
+             marginRight: 130,
+             marginBottom: 25
+    },
+    title: {
+        text: "Tweet Graph"
+    },
+    xAxis: {
+        categories: buildXAxis()
+    },
+    yAxis: {
+        title: {
+            text: 'Tweets'
+        },
+        plotLines: [{
+            value: 0,
+            width: 1,
+            color: '#808080'
+        }]
+    },
+    legend: { style: 'none' },
+    series: [{
+            name: 'Tokyo',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }, {
+            name: 'New York',
+            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+        }, {
+            name: 'Berlin',
+            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+        }, {
+            name: 'London',
+            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+        }]
+});
+*/
